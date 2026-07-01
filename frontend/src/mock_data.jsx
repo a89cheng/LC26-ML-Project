063d55e84367a9d14030d31319234a42b3895dc5
Adrien Cheng <a89cheng@uwaterloo.ca>
2026-06-30 22:43:23 -0400

    Frontend mock data

--- frontend/src/mock_data.jsx
+++ frontend/src/mock_data.jsx
@@ -4,7 +4,17 @@ export const mockHours = Array.from({ length: 168 }, (_, i) => {
     return {
         forecast_hour: date.toISOString(),
         go_no_go: Math.random() > 0.4,
-        p_safe_launch: Math.random(),
+        p_safe_launch: Math.random() * 100,
         predicted_dist_nm: (Math.random() * 12).toFixed(1),
     }
 })
+
+const altitudes = [110, 320, 500, 800, 1000, 1500, 1900, 3200, 4200, 5600, 7200, 9200, 10400, 11800, 13500, 15800, 17700, 19300, 22000]
+
+export const mockAltitudes = altitudes.map(altitude => ({
+    altitude: altitude,
+    windSpeed: altitude < 1000 ? Math.random() * 30 :
+           altitude < 5000 ? Math.random() * 60 :
+           Math.random() * 150,
+    windDirection: Math.floor(Math.random() * 360),
+}))
