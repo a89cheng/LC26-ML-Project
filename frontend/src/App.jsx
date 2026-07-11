894bde9d71b2ab39018430b276f471ceffb3281c
Adrien Cheng <a89cheng@uwaterloo.ca>
2026-07-10 23:38:26 -0400

    App without hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,32 @@
 import Header from "./components/Header"
 import SafetyGauge from "./components/SafetyGauge"
 import TimelineBar from "./components/TimelineBar"
+import MapPanel from "./components/MapPanel"
+import WindProfileChart from "./components/WindProfileChart"
 
 function App() {
     return (
     <div className="min-h-screen bg-[#0a0e1a]">
+        {/*The header*/}
         <Header />
+
+        {/*The top half*/}
         <div className="flex items-stretch">
-            <div className="outline outline-2 outline-blue-100 w-2/3">
-                {/* Map placeholder */}
+            <div className="outline outline-2 outline-blue-100 w-2/3 h-[450px]">
+                <MapPanel />
             </div>
             <div className="w-1/3 flex">
                 <SafetyGauge />
             </div>
         </div>
+
+        {/*The bottom half*/}
         <div>
             <div className=''>
                 <TimelineBar />
+            </div>
+            <div>
+                <WindProfileChart />
             </div>
         </div>
     </div>
